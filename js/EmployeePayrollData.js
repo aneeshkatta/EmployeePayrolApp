bdec21cf26c3a732e893d2db34a6ef25c1ecca35
Aneesh <katta.aneesh597@gmail.com>
2023-01-10 22:26:40 +0530

    added program

--- js/EmployeePayrollData.js
+++ js/EmployeePayrollData.js
@@ -1,43 +1,49 @@
-class EmployeePayrollData{
-    id;
-    name;
-    salary;
-    gender;
-    startDate;
-    constructor(...params){
-        this.id=params[0];
-        this.name=params[1];
-        this.salary=params[2];
-        this.gender=params[3];
-        this.startDate=params[4];
+class EmployeePayrollData {
+   
+    get id() { return this._id; }
+    set id(id) {
+        this._id = id;
     }
-    get id()  {return this.id;}
-    set id(id)  {
-        this._id=id;       
-    }
-    get Profilepic()  {return this.Profilepic;}
-    set Profilepic(Profilepic)  {
-        this.Profilepic=Profilepic;       
-    }
-    get name()  {return this._name;}
-    set name(name)  {
-        let RegexExpofName= RegExp('^[A-Z]{1}[a-z]{3,}$')
-        if(RegexExpofName.test(name))
-        this._name=name;
+    get name() { return this._name; }
+    set name(name) {
+        let RegexExpofName = RegExp('^[A-Z]{1}[a-z]{3,}$')
+        if (RegexExpofName.test(name))
+            this._name = name;
         else throw 'Name is incorrect'
-        
+
     }
-    get salary()  {return this._salary;}
-    set salary(salary)  {this._salary=salary;}
-    get gender()  {return this._gender;}
-    set gender(gender)  {this._gender=gender;}
-    get startDate()  {return this._startDate;}
-    set startDate(startDate)  {this._startDate=startDate;}
-    toString()
-    {
-        const options = { day:'numeric', month:'long', year: 'numeric'};
-        const empjoineddate=this.startDate ===undefined ?"undefined": 
-        this.startDate.toLocaleDateString("en-US",options);
-        return "id="+this.id+", name="+this.name+", salary="+this.salary+", gender="+this.gender+", startDate="+empjoineddate ;
+    get Profilepic() { return this._Profilepic; }
+    set Profilepic(Profilepic) {
+        this.Profilepic = Profilepic;
     }
+    get gender() { return this._gender; }
+    set gender(gender) { this._gender = gender; }
+    get department() { return this._department; }
+    set department(department) { this._department = department; }
+    get salary() { return this._salary; }
+    set salary(salary) { this._salary = salary; }
+    get note() { return this._note; }
+    set note(note) { this._note = note; }  
+    get startDate() { return this._startDate; }
+    set startDate(startDate) { this._startDate = startDate; }
+    toString() {
+        const options = { day: 'numeric', month: 'long', year: 'numeric' };
+        const empjoineddate = !this.startDate ? undefined :
+            this.startDate.toLocaleDateString("en-US", options);
+        return "id=" + this.id + ", name=" + this.name + ", gender=" + this.gender + + ", Profilepic" + Profilepic+", department=" + this.department + ", salary=" + this.salary + ", startDate=" + empjoineddate ;
+    }
+
 }
+
+
+// function CreateAndUpdateStorage(employeePayrollData) {
+//     let employeePayrolllist = new JSON.parse(localStorage.getItem("EmployeePayrolllist"));
+//     if (employeePayrolllist != undefined) {
+//         employeePayrolllist.push(employeePayrollData);
+//     }
+//     else {
+//         employeePayrolllist = [employeePayrollData];
+//     }
+//     alert(employeePayrolllist.toString());
+//     localStorage.setItem("EmployeePayrolllist", JSON.stringify(employeePayrolllist));
+// }
