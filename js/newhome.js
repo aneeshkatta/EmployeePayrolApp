3ca5d5baad2bbcba2d57c6399690099df69cd776
Aneesh <katta.aneesh597@gmail.com>
2023-01-12 21:14:23 +0530

    added program

--- js/newhome.js
+++ js/newhome.js
@@ -1,36 +1,23 @@
 window.addEventListener('DOMContentLoaded',(event)=>{
-    createInnerHtml();
+    createinnerHtml();
 });
-const createInnerHtml=()=>{
-    const headerHtml=`<th></th>    
-    <th>Name</th>
-    <th>Gender</th>
-    <th>Department</th>
-    <th>Salary</th>
-    <th>Start Date</th>
-    <th>Actions</th>`;
-const InnerHtml =`$(headerHtml)
-<tr>
-<th></th>    
-<th>Name</th>
-<th>Gender</th>
-<th>Department</th>
-<th>Salary</th>
-<th>Start Date</th>
-<th>Actions</th>
-</tr>
-<tr>
-<td><img class="profile" alt="" src="../assets/assets/profile-images/Ellipse -2.png"></td>
-<td> Narayan Mahadevan</td>
-<td>Male</td>
-<td><div>HR</div><div>Finance</div></td>
-<td>300000</td>
-<td>1 nov 2020</td>
-<td>
-    <img id="1" onclick="remove(this)" alt="delete" src="../assets/assets/icons/delete-black-18dp.svg">
-    <img id="1" onclick="update(this)" alt="edit" src="../assets/assets/icons/create-black-18dp.svg">
-</td>
-</tr>`
-;
-document.querySelector('#table-display').InnerHtml=InnerHtml;
+//Template literal(string literal) ES6 feature
+function createInnerHtml()
+{
+    const headerHtml= "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
+    let innerHtml = `${headerHtml}
+        <tr>
+            <td><img class="profile" src="../assets/profile-images/Ellipse -2.png" alt="" ></td>
+            <td>Narayan Mahadevan</td>
+            <td>Male</td>
+            <td><div class='dept-label'>HR</div><div class='dept-label'>Finance</div></td>
+            <td>300000</td>
+            <td>1 Nov 2020</td>
+            <td>
+                <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+                <img id="1" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
+            </td>
+        </tr>
+        `;
+    document.querySelector('#table-display').innerHTML=innerHtml;
 };
